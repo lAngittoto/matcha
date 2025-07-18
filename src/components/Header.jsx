ce83018c1e1514902f6b48ec5959c9cbdbc79631
Lan Gitto <git762647@gmail.com>
2025-07-18 20:21:26 +0800

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+const Header = () => {
+  return (
+    <header className="font-sans px-5 py-5 md:px-7 md:py-7 ">
+      <nav className="flex flex-col justify-between md:flex-row lg:flex-row gap-3 ">
+        <div>
+          <h1 className="text-white text-5xl font-bold md:text-6xl cursor-default">
+            Matcha<span className="text-black text-2xl">thea</span>
+          </h1>
+        </div>
+        <ul className="text-white text-[1.1rem] flex flex-row items-end gap-3 lg:text-2xl :gap-7 cursor-pointer w-full justify-around  sm:flex-row   ">
+          <li>Home</li>
+          <li>About</li>
+          <li>Cart</li>
+          <li>Favorites</li>
+        </ul>
+        <div className="flex items-end gap-3 lg:gap-5">
+          <input
+            type="text"
+            className="placeholder-black  bg-[#7ED957]  outline-none px-2 py-2 rounded-2xl lg:px-5 lg:text-[1rem] text-[0.8rem] "
+            placeholder="Search to Order"
+          />
+          <button className=" bg-[#7ED957]  outline-none px-5 py-2 rounded-2xl lg:text-[1rem] text-[0.8rem] cursor-pointer">Enter</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import DailyMatcha from "../assets/images/DailyMatcha.png";
+import IcedMatcha from "../assets/images/IcedMatcha.png";
+const Home = () => {
+  return (
+    <>
+      <section >
+        <div className="flex flex-col items-center justify-center lg:px-5 lg:py-10  md:flex-row sm:flex-col py-10">
+          <img src={DailyMatcha} alt="Daily matcha image" className="w-[500px] md:w-full " />
+          <h1 className="lg:text-8xl md:text-7xl text-7xl text-white flex flex-col lg:flex-row md:flex-row ">
+            You're Daily, <span className="text-black"> Best Matcha</span>
+          </h1>
+          <img src={IcedMatcha} alt="Iced matcha image" className="w-[500px] md:w-full "/>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Home;
