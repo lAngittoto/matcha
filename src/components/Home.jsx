739145e0c68ead576f7f2c3442915a76bc26ff1d
Lan Gitto <git762647@gmail.com>
2025-07-28 17:50:48 +0800

    Done First Project.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="flex flex-col items-center justify-center lg:px-5 lg:py-10  md:flex-row sm:flex-col py-10">
           <img src={DailyMatcha} alt="Daily matcha image" className="w-[500px] md:w-full " />
           <h1 className="lg:text-8xl md:text-7xl text-8xl  text-white flex flex-col lg:flex-row md:flex-row cursor-default ">
-            You're Daily, <span className="text-black"> Best Matcha</span>
+            Your Daily, <span className="text-black"> Best Matcha</span>
           </h1>
           <img src={IcedMatcha} alt="Iced matcha image" className="w-[500px] md:w-full"/>
         </div>
